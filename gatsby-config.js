8bef64ff76a030ea3abacd2e96e50e128f7cdcbe
Evan Willhite <evan@fourkitchens.com>
2020-04-16 10:35:12 -0500

    gatsby scss support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const globImporter = require('node-sass-glob-importer');
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -27,6 +29,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options:{
+        importer: globImporter()
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
